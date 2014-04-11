da445caab53e89648b4411b1d99e22801545a34d
Eric Menendez <ericmenendez@gmail.com>
2014-04-11 15:06:16 -0400

    Works for "Modelling"

--- spreadsheet2labels.py
+++ spreadsheet2labels.py
@@ -4,6 +4,7 @@
 
 from __future__ import print_function
 
+import csv
 import getpass
 import gspread
 
@@ -17,24 +18,35 @@ def main():
     # Open spreadsheet
     Spreadsheet = gc.open("2014 BSC info")
 
+    # Prepare output file
+    with open('output.csv', 'wb') as csvfile:
+        writer = csv.DictWriter(csvfile, ('Category', 'Name'))
+        writer.writeheader()
 
-    # # Fetch a cell range
-    # cell_list = wks.range('A1:B7')
+        for Worksheet in Spreadsheet.worksheets():
+            worksheetPrompt = raw_input('Use %s? [n]: ' % Worksheet.title)
+            if worksheetPrompt.lower().startswith('y'):
+                worksheetPrompt = True
+            else:
+                worksheetPrompt = False
+            if worksheetPrompt:
+                # Parse this spreadsheet into csv
+                rows = Worksheet.row_count
+                cols = Worksheet.col_count
 
-    for Worksheet in Spreadsheet.worksheets():
-        worksheetPrompt = raw_input('Use %s? [n]: ' % Worksheet.title)
-        if worksheetPrompt.lower().startswith('y'):
-            worksheetPrompt = True
-        else:
-            worksheetPrompt = False
-        if worksheetPrompt:
-            # Parse this spreadsheet into csv
-            rows = Worksheet.row_count
-            cols = Worksheet.col_count
+                for col in range(1, Worksheet.col_count):
+                    category = None
 
-            for col in range(1, Worksheet.col_count):
-                values = Worksheet.col_values(col)
-                print(values[0])
+                    for value in Worksheet.col_values(col):
+                        if value and not category:
+                            category = value
+                        elif value:
+                            writer.writerow({'Category': category.encode('ascii', 'replace').replace('?', ' '),
+                                             'Name': value.encode('ascii', 'replace').replace('?', ' ')})
+                        elif not value:
+                            category = None
+
+
 
 
 
