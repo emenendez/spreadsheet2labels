032fa272099e936bf15c32ab622bb42622013a27
Eric Menendez <ericmenendez@gmail.com>
2014-04-28 18:58:44 -0400

    Split into module and script

--- spreadsheet2labels/__init__.py
+++ spreadsheet2labels/__init__.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
-#
-# Based on Google sample code. 
 
 from __future__ import print_function
-
 import csv
 import getpass
 import gspread
 
 __version__ = '0.1.0.dev1'
+
+
+def fromworksheet(Worksheet):
+    # Parse this spreadsheet into list
+    rows = Worksheet.row_count
+    cols = Worksheet.col_count
+
+    for col in range(1, Worksheet.col_count):
+        category = None
+
+        for value in Worksheet.col_values(col):
+            if value and not category:
+                category = value
+            elif value:
+                yield [category.encode('ascii', 'replace').replace('?', ' '),
+                       value.encode('ascii', 'replace').replace('?', ' ')]
+            elif not value:
+                category = None
 
 
 def main():
@@ -23,8 +38,8 @@ def main():
 
     # Prepare output file
     with open('output.csv', 'wb') as csvfile:
-        writer = csv.DictWriter(csvfile, ('Category', 'Name'))
-        writer.writeheader()
+        writer = csv.writer(csvfile)
+        writer.writerow(['Category', 'Name'])
 
         for Worksheet in Spreadsheet.worksheets():
             worksheetPrompt = raw_input('Use %s? [n]: ' % Worksheet.title)
@@ -34,23 +49,7 @@ def main():
                 worksheetPrompt = False
             if worksheetPrompt:
                 # Parse this spreadsheet into csv
-                rows = Worksheet.row_count
-                cols = Worksheet.col_count
-
-                for col in range(1, Worksheet.col_count):
-                    category = None
-
-                    for value in Worksheet.col_values(col):
-                        if value and not category:
-                            category = value
-                        elif value:
-                            writer.writerow({'Category': category.encode('ascii', 'replace').replace('?', ' '),
-                                             'Name': value.encode('ascii', 'replace').replace('?', ' ')})
-                        elif not value:
-                            category = None
-
-
-
+                writer.writerows(fromworksheet(Worksheet))
 
 
 if __name__ == '__main__':
